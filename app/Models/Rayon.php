dcdd0564578856451a2028c9efdfd9e4f1a900f5
hichamoubaha <oubahahicham7@gmail.com>
2025-03-11 10:56:04 +0000

    modify the model of rayon

--- app/Models/Rayon.php
+++ app/Models/Rayon.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Rayon extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nom'];
+
+    public function produits()
+    {
+        return $this->hasMany(Produit::class);
+    }
 }

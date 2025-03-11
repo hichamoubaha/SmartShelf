cd0991ff211bb9e5358105e6b7ef9877482f5573
hichamoubaha <oubahahicham7@gmail.com>
2025-03-11 10:58:20 +0000

    modify the model of product

--- app/Models/Produit.php
+++ app/Models/Produit.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Produit extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nom', 'quantite_stock', 'prix', 'rayon_id'];
+
+    public function rayon()
+    {
+        return $this->belongsTo(Rayon::class);
+    }
 }
